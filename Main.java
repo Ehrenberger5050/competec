e8a996d50206a41339b18f7b4ad49ff2db5d35dd
Ehrenberger5050 <gabrielehrenberger57@gmail.com>
2026-04-18 11:45:06 -0300

    add: Primeiro exercicio Competec

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	float laranja = 10.00f;
+	float uva= 8.00f;
+	float soma= laranja+ uva;
+	System.out.println("laranja + uva = " + (laranja + uva));
+	
+	}
+    
+}
